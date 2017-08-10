763c3bc832fdad73d413b47324e411f55315180e
Pawel Marcinkowski <pmarcinkowski1016@timeinc.com>
2017-08-10 13:44:57 +0100

    Added comments to JS

--- components/scripts/app.js
+++ components/scripts/app.js
@@ -19,6 +19,9 @@ class MainTable extends React.Component {
     }
     sortDays = (e) => {
         e.preventDefault();
+
+        //depend of the state of orderDays map the data array and sort ascending or desceding by resent rank
+
         var orderBy = this.state.orderDays === "asc" ? "desc" : "asc",
             result = this.state.data.sort((a,b) => {
                 return (orderBy === "desc") ? (b["recent"] - a["recent"]) : (a["recent"] - b["recent"]);
@@ -29,9 +32,13 @@ class MainTable extends React.Component {
             orderDays: orderBy,
             orderBy: "days"
         });
-    }
+    } //sort by last 30 days
+
     sortAllTime = (e) => {
         e.preventDefault();
+
+        //depend of the state of orderAll map the data array and sort ascending or desceding by all time rank
+
         var orderBy = this.state.orderAll === "asc" ? "desc" : "asc",
             result = this.state.data.sort((a,b) => {
                 return (orderBy === "desc") ? (b["alltime"] - a["alltime"]) : (a["alltime"] - b["alltime"]);
@@ -41,8 +48,12 @@ class MainTable extends React.Component {
             orderAll: orderBy,
             orderBy: "all"
         });
-    }
+    } //sort by all time rank
+
     componentWillMount() {
+
+        //get the data from external file and set data to it's value
+
         var url = "https://fcctop100.herokuapp.com/api/fccusers/top/recent";
         this.serverRequest = $.get(url, (result) => {
             this.setState({
@@ -52,6 +63,8 @@ class MainTable extends React.Component {
     }
     render() {
         var list = this.state.data;
+
+        // map the data array and return table-row elements (Campers list) with props.
         list = list.map((item, index) => {
             return (
                 <CampersList
@@ -73,12 +86,16 @@ class MainTable extends React.Component {
                             <th>Id</th>
                             <th>Camper Name</th>
                             <th>
+                                { /*After onClick: check if 'orderBy' state is equal to 'days' if yes* add a span to this <th> if not, null
+                                    //*when adding span, check if value is 'asc' or not and add relevant icon*/}
                                 <a href="#" onClick={this.sortDays}>Points in past 30 days {(this.state.orderBy === "days") ?
                                         <span className={(this.state.orderDays === "asc") ? " glyphicon glyphicon-chevron-up" : "glyphicon glyphicon-chevron-down"}></span>
                                     : null}
                                 </a>
                             </th>
                             <th>
+                                { /*After onClick: check if 'orderBy' state is equal to 'all' if yes* add a span to this <th> if not, null
+                                    //*when adding span, check if value is 'asc' or not and add relevant icon*/}
                                 <a href="#" onClick={this.sortAllTime}>All time points {(this.state.orderBy === "all") ?
                                         <span className={(this.state.orderAll === "asc") ? " glyphicon glyphicon-chevron-up" : "glyphicon glyphicon-chevron-down"}></span>
                                     : null}
@@ -93,10 +110,9 @@ class MainTable extends React.Component {
                 <footer className="text-center">
                     <span>Pawel Marcinkowski &copy; 2017</span>
                 </footer>
-
             </div>
         );
-    }
+    } // render
 }
 
 ReactDOM.render(<MainTable />, document.getElementById("main-interface"));
